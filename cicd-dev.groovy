ab6d46f271a71c452cb9232690a22fe704368b4d
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:37:18 -0400

    Change organization to zopencommunity

--- cicd-dev.groovy
+++ cicd-dev.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/antport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/antport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/antport.git'), string(name: 'PORT_DESCRIPTION', value: 'Ant is a Java based build tool.' ), string(name: 'BUILD_LINE', value: 'DEV') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/antport.git'), string(name: 'PORT_DESCRIPTION', value: 'Ant is a Java based build tool.' ), string(name: 'BUILD_LINE', value: 'DEV') ]
   }
 }
